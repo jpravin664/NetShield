52f61a26db2430c8e02022c9ca432d3d84ff3d36
jpravin664 <manopravin1@gmail.com>
2025-07-25 19:59:16 +0530

    Enhanced vulnerability scan with additional Nmap scripts for comprehensive coverage

--- scanner.py
+++ scanner.py
@@ -23,18 +23,24 @@ def scan_ports(target, scan_type="normal"):
             nm.scan(target, arguments='-sS -T4 --top-ports 1000')  # Full scan on top 1000 ports
         elif scan_type == "vuln":
             # Targeted vulnerability scan for Metasploitable 2
-            nm.scan(target, arguments=(
+           nm.scan(target, arguments=(
                 '--script='
                 'ftp-anon,ftp-vsftpd-backdoor,ftp-proftpd-backdoor,'
                 'http-slowloris,http-sql-injection,http-dombased-xss,http-enum,http-config-backup,http-robots.txt,'
+                'http-methods,http-headers,http-title,http-stored-xss,http-shellshock,http-userdir-enum,'
                 'mysql-vuln-cve2012-2122,mysql-empty-password,mysql-info,'
                 'smb-vuln-ms08-067,smb-vuln-ms17-010,smb-vuln-cve-2017-7494,smb-enum-shares,smb-enum-users,'
-                'sshv1,ssh2-enum-algos,'
+                'sshv1,ssh2-enum-algos,ssh-hostkey,'
                 'telnet-brute,telnet-encryption,'
                 'rpcinfo,rpc-grind,nfs-showmount,nfs-statfs,'
-                'vulners,vulscan '
+                'vulners,vulscan,ssl-cert,ssl-enum-ciphers,'
+                'dns-zone-transfer,dns-recursion,'
+                'snmp-info,snmp-interfaces,snmp-netstat,'
+                'smtp-open-relay,smtp-enum-users,'
+                'imap-capabilities,pop3-capabilities'
                 '--script-timeout=30s -T4 --min-parallelism=100 --min-hostgroup=64'
             ))
+
         else:
             nm.scan(target, '1-1024', arguments='-T4')  # Normal scan
 
